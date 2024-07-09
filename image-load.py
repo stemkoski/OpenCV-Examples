3f8cf9cbb411a5b89044ff7116abf14a9130526a
Lee Stemkoski <stemkoski@gmail.com>
2024-07-09 10:22:26 -0400

    color masking example

--- image-load.py
+++ image-load.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import sys
 
 print("running...")
@@ -7,7 +8,7 @@ print("running...")
 img = cv2.imread("images/color-grid.png", -1)
 
 # set to pixel dimensions
-# img2 = cv2.resize( img, (512, 512) )
+img2 = cv2.resize( img, (512, 512) )
 
 # set to scaling value
 # img2 = cv2.resize( img, (0,0), fx=0.5, fy=0.25)
@@ -18,9 +19,23 @@ img = cv2.imread("images/color-grid.png", -1)
 # reflect
 # 0 = around horizontal axis (swap top and bottom)
 # 1 = around vertical axis (swap left and right)
-img2 = cv2.flip(img, 1) 
+# img2 = cv2.flip(img, 1) 
 
-cv2.imshow("ImageWindow", img2)
+# convert from BGR to HSV for simpler color masking
+hsv = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV)
+# get the yellow color mask
+mask = cv2.inRange(hsv, (25, 50, 50), (35, 255, 255))
+# foreground
+fg = cv2.bitwise_and(img2,img2, mask=mask)
+# background
+mask_inv = cv2.bitwise_not(mask)
+bg = cv2.bitwise_and(img2,img2, mask=mask_inv)
+
+
+cv2.imshow("ImageWindow1", img2)
+cv2.imshow("ImageWindow2", mask)
+cv2.imshow("ImageWindow3", fg)
+cv2.imshow("ImageWindow4", bg)
 
 # wait (0 = forever) for next key press; store value of pressed key
 k = cv2.waitKey(0)
